76b33b0f15b71f42d17189428a848be2566d1add
ishworpanta10 <ishworpanta10@gmail.com>
2020-08-12 08:46:08 +0545

    DAte time ADDED

--- src/App.js
+++ src/App.js
@@ -2,8 +2,34 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-const name = "Ishwor Panta";
-const element = <div>Hello , {name}</div>;
+function formatName(user) {
+  if (user) {
+    return "Welcome " + user.fname;
+  }
+  return "Welcome Stranger";
+}
+
+const user = {
+  fname: "Ishwor",
+  lname: "Panta",
+};
+
+// const name = "Ishwor Panta";
+// const element = <div>Hello , {name}</div>;
+const element = (
+  <div>
+    <h1>
+      {formatName(user)}
+    </h1>
+
+    <h2>
+      It is {new Date().toLocaleDateString()}
+      <br>
+      </br>
+      Current Time is {new Date().toLocaleTimeString()}
+    </h2>
+  </div>
+);
 
 function App() {
   return element;
